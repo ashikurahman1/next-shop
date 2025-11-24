fccaeed757a68037db4042d33e4d446ae08680cd
Ashikur Rahman <ashikurahmantuhin@gmail.com>
2025-11-24 12:55:51 +0600

    Update page.jsx

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -1,5 +1,8 @@
 import Link from 'next/link';
 import React from 'react';
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
 export default async function ProductDetails({ params }) {
   const session = await getServerSession(authOptions);
